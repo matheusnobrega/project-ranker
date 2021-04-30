7d4744e5730607a4baeb1c5750435191f2add1e4
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2021-04-29 21:15:47 -0300

    Criando a classe de Projeto

---
+++ Projeto.py
@@ -0,0 +1,11 @@
+class Projeto:
+
+    def __init__(self, nome, descricao, unidade_responsavel, data_inicio, data_termino, qtd_pessoas_alocadas):
+        self.nome = nome
+        self.descricao = descricao
+        self.unidade_responsavel = unidade_responsavel
+        self.data_inicio = data_inicio
+        self.data_termino = data_termino
+        self.qtd_pessoas_alocadas = qtd_pessoas_alocadas
+
+

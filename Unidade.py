176fdd6cf352cb3382ea226a3a4fda6b58ea29ee
matheus8998 <44419328+matheus8998@users.noreply.github.com>
2021-04-30 00:39:04 -0300

    Criado classe de Unidade

---
+++ Unidade.py
@@ -0,0 +1,7 @@
+class Unidade:
+
+    def __init__(self, sigla, nome_completo, setor, qtd_pessoas_alocadas):
+        self.sigla = sigla
+        self.nome_completo = nome_completo
+        self.setor = setor
+        self.qtd_pessoas_alocadas = qtd_pessoas_alocadas
